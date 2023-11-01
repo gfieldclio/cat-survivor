9b2f176f57c77001ad853d30a9e66215cc22d50b
Greg Field <greg.field@clio.com>
2023-11-01 11:33:42 -0700

    Terrain sprite lookup

--- app/scenes/game.rb
+++ app/scenes/game.rb
@@ -1,6 +1,11 @@
 module Scenes
   module Game
     def self.tick(args)
+      render_background(args)
+    end
+
+    def self.render_background(args)
+      args.outputs.sprites << Sprites::Terrain.tile(x: 50, y: 50, type: "hills", key: "ground")
     end
   end
 end
