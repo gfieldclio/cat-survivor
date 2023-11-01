4e1e2b5f6ff017a402a9b4d198f8241641d2f3eb
Sam Markham <sam.markham@clio.com>
2023-11-01 16:36:59 -0700

    Resolve conflicts (?)

--- app/scenes/game.rb
+++ app/scenes/game.rb
@@ -3,14 +3,9 @@ module Scenes
     def self.tick(args)
       render_background(args)
       render_player(args)
-<<<<<<< HEAD
-      render_enemies(args)
-      # handle_input(args)
-=======
 
       args.state.enemies ||= generate_enemies(args)
       move_enemies(args)
->>>>>>> d32cbb5d057d247da9145e9b8a16362d3583edb6
     end
 
     def self.render_background(args)
@@ -34,30 +29,16 @@ module Scenes
     end
 
     def self.render_player(args)
-<<<<<<< HEAD
       # args.outputs.sprites << Sprites::Player.tile(x: 100, y: 100, type: "cat_1", key: "down_still")
       player = Scenes::Game::Player.new(args)
       player.render(args)
       player.handle_movement(args)
       # args.state.player ||= Scenes::Game::Player.new(args)
-    end
 
-    # def self.handle_input(args)
-    #   if args.inputs # change later to make this only for up, down, left, right--other inputs may do a menu or something
-    #     Scenes::Game::PlayerMovement.tick(args)
-    #   end
-    # end
-
-    def self.render_enemies(args)
-      args.state.enemies ||= generate_enemies(args)
-      args.state.enemies.each { |enemy| enemy.render(args) }
-=======
-      args.outputs.sprites << Sprites::Player.tile(x: 100, y: 100, type: "cat_1", key: "down_still")
     end
 
     def self.move_enemies(args)
       args.state.enemies.each { |enemy| enemy.move(args) }
->>>>>>> d32cbb5d057d247da9145e9b8a16362d3583edb6
     end
 
     def self.generate_enemies(args)
