c5c7791f41b32d52a1dce4f70c0372c6c38ebd77
Greg Field <greg.field@clio.com>
2023-11-02 11:26:47 -0700

    Scroll the screen

--- app/scenes/game/player.rb
+++ app/scenes/game/player.rb
@@ -15,10 +15,13 @@ module Scenes::Game
     end
 
     def self.handle_movement(args)
+      dx = 0
+      dy = 0
+
       if args.inputs.keyboard.left
         args.state.player.key = "left_walk_1"
         toggle_on_iteration(args)
-        args.state.player.x -= 3
+        dx -= 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.left
           args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "left_still")
@@ -27,7 +30,7 @@ module Scenes::Game
       elsif args.inputs.keyboard.right
         args.state.player.key = "right_walk_1"
         toggle_on_iteration(args)
-        args.state.player.x += 3
+        dx += 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.right
           args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "right_still")
@@ -36,7 +39,7 @@ module Scenes::Game
       elsif args.inputs.keyboard.up
         args.state.player.key = "up_walk_1"
         toggle_on_iteration(args)
-        args.state.player.y += 3
+        dy += 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.up
           args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "up_still")
@@ -45,7 +48,7 @@ module Scenes::Game
       elsif args.inputs.keyboard.down
         args.state.player.key = "down_walk_1"
         toggle_on_iteration(args)
-        args.state.player.y -= 3
+        dy -= 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.down
           args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "down_still")
@@ -53,6 +56,17 @@ module Scenes::Game
         end
       else
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
+      end
+
+      if args.state.player.x + dx < 100 || args.state.player.x + dx > SCREEN_WIDTH - 100
+        args.state.level.camera.x += dx
+      else
+        args.state.player.x += dx
+      end
+      if args.state.player.y + dy < 100 || args.state.player.y + dy > SCREEN_HEIGHT - 100
+        args.state.level.camera.y += dy
+      else
+        args.state.player.y += dy
       end
     end
   end
