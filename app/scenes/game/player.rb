787ea3a1ffe84233c3026c3b02fd3186d784cae6
Sam Markham <sam.markham@clio.com>
2023-11-01 17:13:43 -0700

    Add still sprites

--- app/scenes/game/player.rb
+++ app/scenes/game/player.rb
@@ -16,18 +16,34 @@ module Scenes::Game
         args.state.player.x -= 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "left_walk_1")
         args.state.player.key = "left_walk_1"
+        if args.inputs.keyboard.key_up.left
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "left_still")
+          args.state.player.key = "left_still"
+        end
       elsif args.inputs.keyboard.right
         args.state.player.x += 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "right_walk_1")
         args.state.player.key = "right_walk_1"
+        if args.inputs.keyboard.key_up.right
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "right_still")
+          args.state.player.key = "right_still"
+        end
       elsif args.inputs.keyboard.up
         args.state.player.y += 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "up_walk_1")
         args.state.player.key = "up_walk_1"
+        if args.inputs.keyboard.key_up.up
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "up_still")
+          args.state.player.key = "up_still"
+        end
       elsif args.inputs.keyboard.down
         args.state.player.y -= 3
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "down_walk_1")
         args.state.player.key = "down_walk_1"
+        if args.inputs.keyboard.key_up.down
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "down_still")
+          args.state.player.key = "down_still"
+        end
       else
         args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
       end
