0336c27a8f27e3948ef6a494511b21ab4634f408
Sam Markham <sam.markham@clio.com>
2023-11-02 10:43:51 -0700

    Add ability to use whichever cat

--- app/scenes/game/player.rb
+++ app/scenes/game/player.rb
@@ -5,6 +5,7 @@ module Scenes::Game
       args.state.player.x ||= 100
       args.state.player.y ||= 100
       args.state.player.key ||= "down_still"
+      args.state.player.cat_type ||= "cat_6"
     end
 
     def self.toggle_on_iteration(args)
@@ -18,40 +19,40 @@ module Scenes::Game
         args.state.player.key = "left_walk_1"
         toggle_on_iteration(args)
         args.state.player.x -= 3
-        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
+        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.left
-          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "left_still")
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "left_still")
           args.state.player.key = "left_still"
         end
       elsif args.inputs.keyboard.right
         args.state.player.key = "right_walk_1"
         toggle_on_iteration(args)
         args.state.player.x += 3
-        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
+        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.right
-          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "right_still")
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "right_still")
           args.state.player.key = "right_still"
         end
       elsif args.inputs.keyboard.up
         args.state.player.key = "up_walk_1"
         toggle_on_iteration(args)
         args.state.player.y += 3
-        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
+        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.up
-          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "up_still")
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "up_still")
           args.state.player.key = "up_still"
         end
       elsif args.inputs.keyboard.down
         args.state.player.key = "down_walk_1"
         toggle_on_iteration(args)
         args.state.player.y -= 3
-        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
+        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
         if args.inputs.keyboard.key_up.down
-          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: "down_still")
+          args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: "down_still")
           args.state.player.key = "down_still"
         end
       else
-        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: "cat_1", key: args.state.player.key)
+        args.outputs.sprites << Sprites::Player.tile(x: args.state.player.x, y: args.state.player.y, type: args.state.player.cat_type, key: args.state.player.key)
       end
     end
   end
