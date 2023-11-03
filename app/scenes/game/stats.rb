8842ebc2c31c8b23263fd5fceb8221ecf969e653
Sam Markham <sam.markham@clio.com>
2023-11-03 12:12:27 -0700

    Add shadows and display stats on bottom of screen

--- app/scenes/game/stats.rb
+++ app/scenes/game/stats.rb
@@ -1,35 +1,167 @@
 module Scenes::Game
   module Stats
     def self.tick(args)
-      args.outputs.labels << {
-        x: 20,
-        y: 700,
+      # Level
+
+      args.outputs.labels << { # shadow 1
+        x: 750 + 1,
+        y: 75,
         text: "Level: " + args.state.player.level.to_s,
         # size specification can be either size_enum or size_px
         size_enum: 10,
         # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
         alignment_enum: 0,
-        r: 155,
-        g: 50,
-        b: 50,
+        r: 0,
+        g: 0,
+        b: 0,
         a: 255,
-        vertical_alignment_enum: 2, # 0 is bottom, 1 is middle, 2 is top
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
         font: "fonts/Abaddon_Bold.ttf"
       }
 
+      args.outputs.labels << { # shadow 2
+        x: 750 - 1,
+        y: 75,
+        text: "Level: " + args.state.player.level.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << { # shadow 3
+        x: 750,
+        y: 75 + 1,
+        text: "Level: " + args.state.player.level.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << { # shadow 4
+        x: 750,
+        y: 75 - 1,
+        text: "Level: " + args.state.player.level.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << { # main text
+        x: 750,
+        y: 75,
+        text: "Level: " + args.state.player.level.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 255,
+        g: 255,
+        b: 255,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      # Kill count
+
       args.outputs.labels << {
-        x: 20,
-        y: 650,
+        x: 900 + 1,
+        y: 75,
         text: "Enemies defeated: " + args.state.kill_count.to_s,
         # size specification can be either size_enum or size_px
         size_enum: 10,
         # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
         alignment_enum: 0,
-        r: 155,
-        g: 50,
-        b: 50,
+        r: 0,
+        g: 0,
+        b: 0,
         a: 255,
-        vertical_alignment_enum: 2, # 0 is bottom, 1 is middle, 2 is top
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << {
+        x: 900 - 1,
+        y: 75,
+        text: "Enemies defeated: " + args.state.kill_count.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << {
+        x: 900,
+        y: 75 + 1,
+        text: "Enemies defeated: " + args.state.kill_count.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << {
+        x: 900,
+        y: 75 - 1,
+        text: "Enemies defeated: " + args.state.kill_count.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
+        font: "fonts/Abaddon_Bold.ttf"
+      }
+
+      args.outputs.labels << {
+        x: 900,
+        y: 75,
+        text: "Enemies defeated: " + args.state.kill_count.to_s,
+        # size specification can be either size_enum or size_px
+        size_enum: 10,
+        # 0 represents "left aligned". 1 represents "center aligned". 2 represents "right aligned".
+        alignment_enum: 0,
+        r: 255,
+        g: 255,
+        b: 255,
+        a: 255,
+        vertical_alignment_enum: 0, # 0 is bottom, 1 is middle, 2 is top
         font: "fonts/Abaddon_Bold.ttf"
       }
 
