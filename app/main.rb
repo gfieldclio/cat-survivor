4afc8c209c8df9b66cbc9a1d06bc6da672c7d147
Sam Markham <sam.markham@clio.com>
2023-11-07 17:03:06 -0800

    Pause the game when screen is not in focus

--- app/main.rb
+++ app/main.rb
@@ -11,21 +11,41 @@ require "app/weapons/laser"
 require "app/weapons/feather"
 
 def tick args
-  args.outputs.solids << [0, 0, 1280, 720, 0, 0, 0]
+  # if the keyboard doesn't have focus, and it isn't the first tick
+  if !args.inputs.keyboard.has_focus && args.state.tick_count != 0
+    args.outputs.background_color = [0, 0, 0]
+    args.outputs.labels << {
+      x: 640,
+      y: 360,
+      text: "Game Paused (click to resume).",
+      size_enum: 10,
+      alignment_enum: 1,
+      r: 255,
+      g: 255,
+      b: 255,
+      font: "fonts/Abaddon_Bold.ttf"
+    }
+    args.audio[:bg_music].paused = true
+  else
+    args.audio[:bg_music].paused = false
 
-  if args.state.scene.nil?
-    args.state.scene = :intro
-    args.audio[:bg_music] = { input: "audio/music/abstraction-sanctuary.ogg", looping: true, gain: 0.1 }
-  end
+    # perform your regular tick function
+    args.outputs.solids << [0, 0, 1280, 720, 0, 0, 0]
 
-  case args.state.scene
-  when :intro
-    Scenes::Intro.tick(args)
-  when :game
-    Scenes::Game.tick(args)
-  when :game_over
-    Scenes::GameOver.tick(args)
-  when :credits
-    Scenes::Credits.tick(args)
+    if args.state.scene.nil?
+      args.state.scene = :intro
+      args.audio[:bg_music] = { input: "audio/music/abstraction-sanctuary.ogg", looping: true, gain: 0.1 }
+    end
+
+    case args.state.scene
+    when :intro
+      Scenes::Intro.tick(args)
+    when :game
+      Scenes::Game.tick(args)
+    when :game_over
+      Scenes::GameOver.tick(args)
+    when :credits
+      Scenes::Credits.tick(args)
+    end
   end
 end
